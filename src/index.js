b2f34b1f825633cdf4a793d43ddc95d38587131d
shiv-chan <kshibuya31@gmail.com>
2021-06-14 21:26:49 -0700

    Add scss file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import WeatherApp from './components/WeatherApp';
 
 ReactDOM.render(<WeatherApp />, document.getElementById('root'));
